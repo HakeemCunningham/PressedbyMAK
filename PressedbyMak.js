48b3edc9d29d1e4b4b44d0aa978221e0b1de479d
HakeemCunningham <96095882+HakeemCunningham@users.noreply.github.com>
2023-04-21 03:03:54 +0000

    modified js and css

--- PressedbyMak.js
+++ PressedbyMak.js
@@ -1,13 +1,13 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navmenu = document.querySelector('.nav-menu');
 
 hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
+        hamburger.classList.toggle('active');
+        navmenu.classList.toggle('active');
     })
 
-document.querySelectorAll(".nav-link").forEach(n => n.
-addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach(n => n.
+addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navmenu.classList.remove('active');
 }))
