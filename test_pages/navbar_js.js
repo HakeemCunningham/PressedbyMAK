d467cc71c61c743e1c41a1ae4d4a0bc17f717756
HakeemCunningham <96095882+HakeemCunningham@users.noreply.github.com>
2023-04-26 19:27:00 +0000

    created code for adjusted navbar and quote page

---
+++ test_pages/navbar_js.js
@@ -0,0 +1,23 @@
+//Hamburger menu transition//
+const hamburger = document.querySelector('.hamburger');
+const navmenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle('active');
+        navmenu.classList.toggle('active');
+    })
+
+document.querySelectorAll('.nav-link').forEach(n => n.
+addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navmenu.classList.remove('active');
+}))
+
+//Sticky Navbar//
+$(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+        $('.navbar').addClass('.sticky')
+    } else{
+        $('.navbar').removeClass('.sticky')
+    }
+});
